c4fbc565983c34819309bbd56f71746a43741601
ayane04 <gomiayanr@gmail.com>
2022-02-17 18:16:25 -0800

    change images

--- app/src/main/java/com/application/app1/MainActivity.kt
+++ app/src/main/java/com/application/app1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.application.app1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -17,10 +18,26 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val randomInt = (1..6).random()
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
+
+//        Toast.makeText(this, "button clicked",
+//            Toast.LENGTH_SHORT).show()
         val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "Dice Rolled!"
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+//        resultText.text = "Dice Rolled!"
         resultText.text = randomInt.toString()
+
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+
     }
+
+
 }
